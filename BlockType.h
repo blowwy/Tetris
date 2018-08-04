6e46b847926c3131658fc459b569407e66bf00f1
blowwy <spacemanblowwy@gmail.com>
2018-08-04 17:19:50 +0300

    added block realistaion + move block method

---
+++ BlockType.h
@@ -0,0 +1,13 @@
+//
+// Created by boy on 04.08.18.
+//
+
+#ifndef UNTITLED_BLOCKTYPES_H
+#define UNTITLED_BLOCKTYPES_H
+enum class BlockTypes{
+    SQUARE,
+    STRAIGHT,
+    LSHAPED,
+    ZSHAPED,
+};
+#endif //UNTITLED_BLOCKTYPES_H
---
+++ MoveType.h
@@ -0,0 +1,8 @@
+//
+// Created by boy on 04.08.18.
+//
+
+#ifndef UNTITLED_MOVETYPE_H
+#define UNTITLED_MOVETYPE_H
+
+#endif //UNTITLED_MOVETYPE_H
